1e04984df29a8490f63c011875d14672210bc503
Lucas Famous <lucas.j.famous@gmail.com>
2025-11-28 01:29:32 -0500

    chi-square

--- negative_selection.py
+++ negative_selection.py
@@ -1,5 +1,6 @@
 import random
 import time
+import copy
 from statistical_validation import statisticalValidation
 import tqdm
 from normalization import normalize_data
@@ -8,23 +9,26 @@ from normalization import normalize_data
 # ref_table = reference table, tar_table = target table
 def detect_anomalies(ref_table, tar_table, features, detector_count, matching_threshold=None, method='chi_square', normal=False):
     start = time.time()
+    unnormalized_ref_table = copy.deepcopy(ref_table) # needed to detect date and categorical values during validation
+    unnormalized_tar_table = copy.deepcopy(tar_table)
     attempts = 0
     detectors = []
-    max_attempts = detector_count * 1000
+    max_attempts = detector_count * 100
     if matching_threshold is None:
-        matching_threshold = (len(features) * 0.15) ** 0.5
+        avg_dist = 0.28
+        matching_threshold = ((avg_dist**2) * len(features)) ** 0.5
 
     # normalize and convert data to numbers to calculate distance
     print("Normalizing data...")
-    ref_table, tar_table = normalize_data(ref_table, tar_table, features)
+    ref_table_norm, tar_table_norm = normalize_data(ref_table, tar_table, features)
 
     # generate detectors
     while len(detectors) < detector_count and attempts < max_attempts:
-        if (attempts % 1000) == 0:
+        if (attempts % 100) == 0:
             print(f"\rGenerating detectors: {len(detectors)}/{detector_count} attempts {attempts}/{max_attempts}", end="")
         isValid = True
         candidate = generateRandomDetector(features)
-        for record in sampleFrom(ref_table, 1000):
+        for record in sampleFrom(ref_table_norm, 1000):
             distance = calculateDistance(candidate, record)
             if distance < matching_threshold:
                 isValid = False
@@ -41,9 +45,9 @@ def detect_anomalies(ref_table, tar_table, features, detector_count, matching_th
     # find anomalies
     anomalies = []
     chunk_size = 10000
-    size_of_t = len(tar_table)
+    size_of_t = len(tar_table_norm)
     for i in tqdm.tqdm(range(0, size_of_t, chunk_size), desc="Detecting anomalies"):
-        chunk = tar_table[i:i + chunk_size]
+        chunk = tar_table_norm[i:i + chunk_size]
         for record in chunk:
             for detector in detectors:
                 distance = calculateDistance(detector, record)
@@ -56,7 +60,7 @@ def detect_anomalies(ref_table, tar_table, features, detector_count, matching_th
     with open('results.txt', 'a') as f:
         f.write(f"The NSA algorithm took {end - start} seconds and found {len(anomalies)} anomalies.\n")
 
-    A = statisticalValidation(anomalies, ref_table, tar_table, method=method, normal=normal)
+    A = statisticalValidation(anomalies, ref_table_norm, tar_table_norm, unnormalized_ref_table, unnormalized_tar_table, method=method, normal=normal)
     return A
 
 # make random numbers for each feature
--- statistical_validation.py
+++ statistical_validation.py
@@ -1,8 +1,11 @@
 import time
 import tqdm
 import results
+import decimal
+import datetime
+from scipy import stats
 
-def statisticalValidation(anomalies, ref_table, tar_table, method='chi_square', normal=False):
+def statisticalValidation(anomalies, ref_table, tar_table, unnormalized_ref, unnormalized_tar, method='chi_square', normal=False):
     print("Results before validation:")
     if (normal):
         results.check_anomalies(anomalies, tar_table, tar_table)
@@ -14,13 +17,14 @@ def statisticalValidation(anomalies, ref_table, tar_table, method='chi_square', 
     result = None
     match method:
         case 'chi_square':
-            result = chi_square_validation(anomalies, ref_table, tar_table)
+            categorical_features = check_categorical_columns(ref_table)
+            result = chi_square_validation(anomalies, unnormalized_ref, unnormalized_tar, categorical_features)
         case 'Z_score':
             result = Z_score_validation(anomalies, ref_table)
         case 'IQR':
             result = IQR_validation(anomalies, ref_table)
         case 'range_check':
-            result = range_check(anomalies, ref_table)
+            result = range_check(anomalies, unnormalized_ref)
         case 'threshold':
             result = threshold_validation(anomalies, ref_table, tar_table)
         case 'monte_carlo':
@@ -29,20 +33,91 @@ def statisticalValidation(anomalies, ref_table, tar_table, method='chi_square', 
             print(f"Unknown statistical validation method: {method}")
             return anomalies
     end_time = time.time()
-    print(f"Statistical validation using {method} took {end_time - start_time} seconds and validated {len(result)} anomalies.")
-    with open('results.txt', 'a') as f:
-        f.write(f"Statistical validation using {method} took {end_time - start_time} seconds and validated {len(result)} anomalies.\n")
-    print("Results after validation:")
-    if (normal):
-        results.check_anomalies(result, tar_table, tar_table)
+    text = ""
+    if(type(result) == bool):
+        if (result is True):
+            text = f"Statistical validation using {method} took {end_time - start_time} seconds and validated anomalies."
+        elif (result is False):
+            text = f"Statistical validation using {method} took {end_time - start_time} seconds did not detect a difference."
+        with open('results.txt', 'a') as f:
+            f.write(f"{text}\n")
+        print(text)
     else:
-        results.check_anomalies(result, ref_table, tar_table)
+        text = f"Statistical validation using {method} took {end_time - start_time} seconds and validated {len(result)} anomalies."
+        with open('results.txt', 'a') as f:
+            f.write(f"{text}\n")
+        print("Results after validation:")
+        if (normal):
+            results.check_anomalies(result, tar_table, tar_table)
+        else:
+            results.check_anomalies(result, ref_table, tar_table)
     return result
 
 # for categorical datasets
-def chi_square_validation(anomalies, ref_table, tar_table):
-    a = 0.05
-    pass
+# return true if distribution has changed, false otherwise
+def chi_square_validation(anomalies, ref_table, tar_table, categorical_features, a_val=0.05):
+    if (len(anomalies) == 0):
+        return False
+    else:
+        ref_sample_size = len(ref_table)
+        tar_sample_size = len(tar_table)
+        total_sample_size = ref_sample_size + tar_sample_size
+        for feature in categorical_features:
+            ref_count = {} # ref table
+            tar_count = {} # tar table
+            for record in ref_table:
+                value = record[feature]
+                if value not in ref_count:
+                    ref_count[value] = 1
+                else:
+                    ref_count[value] += 1
+            for record in tar_table:
+                value = record[feature]
+                if value not in tar_count:
+                    tar_count[value] = 1
+                else:
+                    tar_count[value] += 1
+            max_values = 30
+            if (len(ref_count) > max_values or len(tar_count) > max_values):
+                text = f"Feature {feature} has too many categories for chi-square test, skipping."
+                with open('results.txt', 'a') as f:
+                    f.write(f"{text}\n")
+                print(text)
+                continue
+            all_values = set(list(ref_count.keys()) + list(tar_count.keys()))
+            total_chi = 0
+            degrees_of_freedom = len(all_values) - 1
+            # ref values
+            ref_column_marginal = sum(ref_count.values())
+            for value in all_values:
+                row_marginal = ref_count.get(value, 0) + tar_count.get(value, 0)
+                expected = (row_marginal * ref_column_marginal) / total_sample_size
+                observed = ref_count.get(value, 0)
+                chi = calc_chi_square(observed, expected)
+                total_chi += chi
+            # tar values
+            tar_column_marginal = sum(tar_count.values())
+            for value in all_values:
+                row_marginal = ref_count.get(value, 0) + tar_count.get(value, 0)
+                expected = (row_marginal * tar_column_marginal) / total_sample_size
+                observed = tar_count.get(value, 0)
+                chi = calc_chi_square(observed, expected)
+                total_chi += chi
+            p = 1 - stats.chi2.cdf(total_chi, degrees_of_freedom)
+            text = f"Feature {feature} Chi-square total: {total_chi}, degrees of freedom: {degrees_of_freedom}, p-value: {p}"
+            with open('results.txt', 'a') as f:
+                f.write(f"{text}\n")
+            print(text)
+            if p < a_val:
+                return True
+    return False
+
+def calc_chi_square(observed, expected):
+    if expected == 0:
+        return 0
+    difference = observed - expected
+    difference_squared = difference ** 2
+    return difference_squared / expected
 
 # for numerical dataset, need to be log transformed if skewed
 def Z_score_validation(anomalies, tar_table):
@@ -122,7 +197,8 @@ def range_check(anomalies, ref_table):
     feature_mins = {}
     feature_maxs = {}
     real_anomalies = []
-    for feature in tqdm.trange(amount_of_features, desc="Finding min/max values"):
+    date_cols = check_date_columns(ref_table)
+    for feature in date_cols:
         for record in ref_table:
             if (record[feature] is not None):
                 if feature not in feature_mins:
@@ -142,8 +218,31 @@ def range_check(anomalies, ref_table):
 
     return real_anomalies
 
+def check_date_columns(table):
+    number_of_features = len(table[0])
+    record = table[0]
+    date_cols = []
+    for feature in range(number_of_features):
+        if (record[feature] is not None):
+            if isinstance(record[feature], datetime.datetime) or isinstance(record[feature], datetime.date):
+                date_cols.append(feature)
+    return date_cols
+
+def check_categorical_columns(table):
+    number_of_features = len(table[0])
+    record = table[0]
+    categorical_cols = []
+    for feature in range(number_of_features):
+        if (record[feature] is not None):
+            if type(record[feature]) == str or type(record[feature]) == bool:
+                categorical_cols.append(feature)
+    return categorical_cols
+
 # check distribution change over 5%
 def threshold_validation(anomalies, ref_table, tar_table):
+    # convert to categorical ranges and call chi_square_validation
+    # for each numerical feature, create 10 bins
+    num_features = len(ref_table[0])
     pass
 
 # our methods
